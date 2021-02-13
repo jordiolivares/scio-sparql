b0dfcb743a13874c14728549416d05f30551d0ad
Jordi Olivares Provencio <jordi@jolivar.es>
2021-02-13 22:21:30 +0100

    Add some more test cases to fulfill one day

--- src/test/scala/FilterTest.scala
+++ src/test/scala/FilterTest.scala
@@ -52,4 +52,45 @@ class FilterTest extends SparqlPipelineTest {
         |""".stripMargin
     testSparql("values.ttl", RDFFormat.TURTLE, query)
   }
+
+  it should "work even without sharing variables" in {
+    val query =
+      """
+        |SELECT *
+        |{ 
+        |  ?s ?p ?o
+        |  FILTER NOT EXISTS { ?x ?y ?z }
+        |}""".stripMargin
+    testSparql("values.ttl", RDFFormat.TURTLE, query)
+  }
+
+  it should "work with inner filter" in {
+    val query =
+      """
+        |PREFIX : <http://example.com/>
+        |SELECT * WHERE {
+        |        ?x :p ?n
+        |        FILTER NOT EXISTS {
+        |                ?x :q ?m .
+        |                FILTER(?n = ?m)
+        |        }
+        |}""".stripMargin
+    testSparql("inner_filter.ttl", RDFFormat.TURTLE, query)
+  }
+
+  it should "work when given a MINUS" in {
+    val query =
+      """
+        |PREFIX :       <http://example/>
+        |PREFIX foaf:   <http://xmlns.com/foaf/0.1/>
+        |
+        |SELECT DISTINCT ?s
+        |WHERE {
+        |   ?s ?p ?o .
+        |   MINUS {
+        |      ?s foaf:givenName "Bob" .
+        |   }
+        |}""".stripMargin
+    testSparql("minus.ttl", RDFFormat.TURTLE, query)
+  }
 }
