ead8a343f7b98d7182637dbbdb65e2445c2ea9d0
Jordi Olivares Provencio <jordi@jolivar.es>
2021-03-04 23:02:58 +0100

    Add some more tests to property paths

--- src/test/scala/PropertyPathsTest.scala
+++ src/test/scala/PropertyPathsTest.scala
@@ -9,4 +9,22 @@ class PropertyPathsTest extends SparqlPipelineTest {
         |{  ?s :item/:price ?x . }""".stripMargin
     testSparql("property_paths.ttl", RDFFormat.TURTLE, query)
   }
+
+  it should "work if the path is negated" in {
+    val query =
+      """
+        |PREFIX :   <http://example/>
+        |SELECT * 
+        |{  ?s !:item ?x . }""".stripMargin
+    testSparql("property_paths.ttl", RDFFormat.TURTLE, query)
+  }
+
+  it should "work if the path is with alternatives" in {
+    val query =
+      """
+        |PREFIX :   <http://example/>
+        |SELECT * 
+        |{  ?s (:item | :price) ?x . }""".stripMargin
+    testSparql("property_paths.ttl", RDFFormat.TURTLE, query)
+  }
 }
